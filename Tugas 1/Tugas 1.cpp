c56b128d91940d8a7e7e2faacd910696379eded5
unknown <faisalmumtaz873@gmail.com>
2024-10-11 21:17:04 +0700

    first commit

---
+++ Tugas 1/Tugas 1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+
+    printf("Nama : Muhammad Faisal Mumtaz\n");
+    printf("Nim : 2441072\n");
+    printf("Jurusan : Teknik Informatika\n");
+
+    return 0;
+}
---
+++ Tugas 1/Tugas 2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+
+    int A, B;
+    A = 10;
+    B = 7;
+
+    printf("Jadi Luas Persegi Panjang dari Ukuran Panjang 10 cm dan Lebar 7 cm adalah\n");
+    printf("%i", (A*B));
+    return 0;
+}
